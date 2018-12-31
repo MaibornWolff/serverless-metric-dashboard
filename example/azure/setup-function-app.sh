f5db6560025e88b4f9417cbac96bc93c06a75b26
Roman Buchholz <roman.buchholz@maibornwolff.de>
2018-12-31 19:05:08 +0100

    Fixed wrong white space

--- example/azure/setup-function-app.sh
+++ example/azure/setup-function-app.sh
@@ -4,13 +4,13 @@ storageConnectionString=$(az storage account show-connection-string --name $stor
 
 az storage container create --name function-releases --public-access off --connection-string $storageConnectionString
 
-fileName = functions_release.zip
-version = v0.1.0
+fileName=functions_release.zip
+version=v0.1.0
 curl -L https://github.com/MaibornWolff/serverless-metric-dashboard/releases/download/$version/functions.zip > $fileName
 
 az storage blob upload --connection-string $storageConnectionString --container-name function-releases --name $fileName --file $fileName
 
-releaseURL = az storage blob url --connection-string $storageConnectionString --container-name function-releases --name $fileName | cut -d '"' -f2
+releaseURL=az storage blob url --connection-string $storageConnectionString --container-name function-releases --name $fileName | cut -d '"' -f2
 
 az functionapp config appsettings set --resource-group $resourceGroup --name $functionAppName --settings WEBSITE_NODE_DEFAULT_VERSION=$nodeVersion WEBSITE_RUN_FROM_PACKAGE=1 PROMETHEUS_AGGREGATOR_ADDRESS=$prometheusAggregatorAddress PUSHGATEWAY_ADDRESS=$pushgatewayAddress  COSMOSDB_CONNECTION=$cosmosConnectionString WEBSITE_RUN_FROM_ZIP=$releaseURL
 
